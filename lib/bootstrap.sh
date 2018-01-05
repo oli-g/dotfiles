04a27d441d6b2dc473b81543b72cf5214506a5cb
Giannicola Olivadoti <olinicola@gmail.com>
2018-01-05 12:34:49 +0100

    Install and setup go

--- lib/bootstrap.sh
+++ lib/bootstrap.sh
@@ -34,7 +34,7 @@ run_bootstrap() {
     # source "${DOTFILES_HOME}/lib/aws/install.sh"
     # source "${DOTFILES_HOME}/lib/heroku/install.sh"
 
-    # source "${DOTFILES_HOME}/lib/go/install.sh"
+    source "${DOTFILES_HOME}/lib/go/install.sh"
     source "${DOTFILES_HOME}/lib/iterm/install.sh"
 
     ask_confirmation "Applying MacOS Defaults for Developers..."
--- lib/go/install.sh
+++ lib/go/install.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 
-if ! formula_exists "go"; then
-    e_header "Installing go..."
-    brew install go --with-cc-common
-    if ! formula_exists "hg"; then
-        e_header "Installing mercurial..."
-        brew install hg
-    fi
+GOPATH="${WORKSPACE_HOME}/go"
+GOROOT="/usr/local/opt/go/libexec"
 
-    export GOPATH="${PROJECTS_PATH}/go"
-    export GOROOT="/usr/local/opt/go/libexec"
-    if [[ ! -d "${GOPATH}/bin" ]]; then
-        mkdir -p "${GOPATH}/bin"
-    fi
-    export PATH="${GOPATH}/bin":$PATH:"${GOROOT}/bin"
+mkdir -p "${GOPATH}/bin" "${GOPATH}/pkg" "${GOPATH}/src"
 
-    e_header "Installing godoc..."
-    go get golang.org/x/tools/cmd/godoc
-    e_header "Installing vet..."
-    go get golang.org/x/tools/cmd/vet
-    e_header "Installing godep"
-    go get github.com/tools/godep
-else
-    ask_confirmation "Updating go..."
-    if is_confirmed; then
-        brew upgrade go
-    fi
-    ask_confirmation "Updating mercurial..."
-    if is_confirmed; then
-        brew upgrade hg
-    fi
-fi
+# if ! formula_exists "go"; then
+#     e_header "Installing go..."
+#     brew install go --with-cc-common
+#     if ! formula_exists "hg"; then
+#         e_header "Installing mercurial..."
+#         brew install hg
+#     fi
+# 
+#     export GOPATH="${PROJECTS_PATH}/go"
+#     export GOROOT="/usr/local/opt/go/libexec"
+#     if [[ ! -d "${GOPATH}/bin" ]]; then
+#         mkdir -p "${GOPATH}/bin"
+#     fi
+#     export PATH="${GOPATH}/bin":$PATH:"${GOROOT}/bin"
+# 
+#     e_header "Installing godoc..."
+#     go get golang.org/x/tools/cmd/godoc
+#     e_header "Installing vet..."
+#     go get golang.org/x/tools/cmd/vet
+#     e_header "Installing godep"
+#     go get github.com/tools/godep
+# else
+#     ask_confirmation "Updating go..."
+#     if is_confirmed; then
+#         brew upgrade go
+#     fi
+#     ask_confirmation "Updating mercurial..."
+#     if is_confirmed; then
+#         brew upgrade hg
+#     fi
+# fi
