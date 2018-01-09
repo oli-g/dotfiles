5621efd76a261b0ef0f28d38f4c8bd181a3aadae
Giannicola Olivadoti <olinicola@gmail.com>
2018-01-09 16:37:22 +0100

    Add Xcode, chruby and ruby-install to Brewfile

--- lib/ruby/install.sh
+++ lib/ruby/install.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-if ! formula_exists "chruby"; then
-    e_header "Installing chruby..."
-    brew install chruby
-else
-    ask_confirmation "Updating chruby..."
-    if is_confirmed; then
-        brew upgrade chruby
-    fi
-fi
+# if ! formula_exists "chruby"; then
+#     e_header "Installing chruby..."
+#     brew install chruby
+# else
+#     ask_confirmation "Updating chruby..."
+#     if is_confirmed; then
+#         brew upgrade chruby
+#     fi
+# fi
 
-if ! formula_exists "ruby-install"; then
-    e_header "Installing ruby-install..."
-    brew install ruby-install
-else
-    ask_confirmation "Updating ruby-install..."
-    if is_confirmed; then
-        brew upgrade ruby-install
-    fi
-fi
+# if ! formula_exists "ruby-install"; then
+#     e_header "Installing ruby-install..."
+#     brew install ruby-install
+# else
+#     ask_confirmation "Updating ruby-install..."
+#     if is_confirmed; then
+#         brew upgrade ruby-install
+#     fi
+# fi
